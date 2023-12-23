578f33d0203298f27c90337b5393d7a4d6c41c32
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-12-22 19:22:52 -0800

    int

---
+++ fxmanifest.lua
@@ -0,0 +1,53 @@
+-- FXVersion Version
+fx_version 'adamant'
+games { 'gta5' }
+
+-- Client Scripts
+client_script 'client/main.lua'
+
+-- Server Scripts
+server_script 'server/main.lua'
+
+-- NUI Default Page
+ui_page "client/html/index.html"
+
+-- Files needed for NUI
+-- DON'T FORGET TO ADD THE SOUND FILES TO THIS!
+files {
+    'client/html/index.html',
+    'client/html/sounds/demo.ogg',
+    'client/html/sounds/Fueling.ogg',
+    'client/html/sounds/putpumpback.ogg',
+    'client/html/sounds/takepumpcar.ogg',
+    'client/html/sounds/putincar.ogg',
+    'client/html/sounds/911call.ogg',
+    'client/html/sounds/buildingLock.ogg',
+    'client/html/sounds/doorBuzzer.ogg',
+    'client/html/sounds/garageDoor.ogg',
+    'client/html/sounds/cellDoor.ogg',
+    'client/html/sounds/shiftreverse.ogg',
+    'client/html/sounds/shiftdrive.ogg',
+    'client/html/sounds/stopped.ogg',
+    'client/html/sounds/panic.ogg',
+    'client/html/sounds/fishing.ogg',
+    'client/html/sounds/or1.ogg',
+    'client/html/sounds/or2.ogg',
+    'client/html/sounds/or3.ogg',
+    'client/html/sounds/or4.ogg',
+    'client/html/sounds/or5.ogg',
+    'client/html/sounds/or6.ogg',
+    'client/html/sounds/or7.ogg',
+    'client/html/sounds/or8.ogg',
+    'client/html/sounds/qp1.ogg',
+    'client/html/sounds/qp2.ogg',
+    'client/html/sounds/qp3.ogg',
+    'client/html/sounds/qp4.ogg',
+    'client/html/sounds/qp5.ogg',
+    'client/html/sounds/qp6.ogg',
+    'client/html/sounds/qp7.ogg',
+    'client/html/sounds/qp8.ogg',
+    'client/html/sounds/qp9.ogg',	
+    'client/html/sounds/qp10.ogg',	
+    'client/html/sounds/qp11.ogg',
+    'client/html/sounds/qp12.ogg'
+}
